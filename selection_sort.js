a0b4dadca0db69c0fed4ddb1293d598f92eb0e68
SeojinSeojin <1106laura@naver.com>
2020-09-30 02:20:51 +0900

    선택 정렬(selection sort)

---
+++ selection_sort.js
@@ -0,0 +1,50 @@
+const numbers = document.querySelector("#numbers");
+const startbtn = document.querySelector("#startbtn");
+const nRange = 8000;
+const nNumb = 200;
+
+const init = () => {
+    for (let i = 0; i < nNumb; i++) {
+        const p = document.createElement("p");
+        const n = Math.ceil(Math.random() * nRange);
+        p.textContent = n;
+        numbers.appendChild(p);
+    }
+};
+
+const delaySec = 1600;
+
+const selectionSort = () => {
+    const p = document.querySelectorAll("p");
+    let tempi, tempmind;
+    console.log(p);
+    console.log(Number(p[0].innerText));
+    for (let i = 0; i < nNumb; i++) {
+        (function(ii) {
+            setTimeout(() => {
+                let min = nRange + 1;
+                let minIndex = -1;
+                for (let j = i; j < nNumb; j++) {
+                    if (Number(p[j].innerText) < min) {
+                        min = Number(p[j].innerText);
+                        minIndex = j;
+                    }
+                }
+                p[i].style.backgroundColor = "#f6ad55";
+                p[minIndex].style.backgroundColor = "#f6ad55";
+                setTimeout(() => {
+                    let temp = Number(p[i].innerText);
+                    p[i].innerText = Number(p[minIndex].innerText);
+                    p[minIndex].innerText = temp;
+                    setTimeout(() => {
+                        p[i].style.backgroundColor = "white";
+                        p[minIndex].style.backgroundColor = "white";
+                    }, 700);
+                }, 700);
+            }, ii * delaySec);
+        })(i);
+    }
+};
+
+init();
+startbtn.addEventListener("click", selectionSort);
